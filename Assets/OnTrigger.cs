945e81656f3322da03ceb73f360ce630a9c61f77
pcelaya <125652831+pcelaya@users.noreply.github.com>
2025-05-11 01:05:30 +0200

    Piano 2/2

--- Assets/OnTrigger.cs
+++ Assets/OnTrigger.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class OnTrigger : MonoBehaviour
 {
+    [SerializeField] public AudioSource audioSource;
+
     object OnTriggerEnter(Collider other)
     {
         // Check if the object that entered the trigger is tagged as "Player"
         if (other.CompareTag("Player"))
         {
-            // Log a message to the console
             Debug.Log("Player has entered the trigger zone!");
+            audioSource.Play();
         }
         return null;
     }
