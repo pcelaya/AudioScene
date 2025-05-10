3b5c2608b7b9c7d7d5356c2b5b56ba01f1ac9144
faelion <101017492+faelion@users.noreply.github.com>
2025-05-11 01:30:38 +0200

    skeleton walking in circles

---
+++ Assets/SkeletonController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float circleRadius = 5f;
+
+    private Vector3 centerPosition;
+    private float angle;
+
+    private void Start()
+    {
+        centerPosition = transform.position;
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", 0.5f);
+        }
+    }
+
+    private void Update()
+    {
+        MoveInCircle();
+        UpdateRotation();
+    }
+
+    private void MoveInCircle()
+    {
+        angle += speed * Time.deltaTime;
+
+        float x = Mathf.Cos(angle) * circleRadius;
+        float z = Mathf.Sin(angle) * circleRadius;
+
+        transform.position = centerPosition + new Vector3(x, 0, z);
+    }
+
+    private void UpdateRotation()
+    {
+        // Calcular dirección tangente al círculo
+        Vector3 tangentDirection = new Vector3(-Mathf.Sin(angle), 0, Mathf.Cos(angle));
+        transform.rotation = Quaternion.LookRotation(tangentDirection);
+    }
+}
